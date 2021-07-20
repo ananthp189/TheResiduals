c96d8304e47da5b9db3a3aa85eca63b39dd5afa6
Klementina Mezesova <77467710+klemeze@users.noreply.github.com>
2021-07-20 14:46:07 +0200

    (R Script) Tidying Script

---
+++ TidyTuesdayScript.R
@@ -0,0 +1,46 @@
+# Load the data
+hike_data <- readr::read_rds(url('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-11-24/hike_data.rds'))
+
+# Libraries
+library(rvest)
+library(tidyverse)
+library(here)
+library(ggplot2)
+library(plotly)
+
+# Cleaning script (by TidyTuesday)
+clean_hike_data <- hike_data %>% 
+  mutate(
+    trip = case_when(
+      grepl("roundtrip",length) ~ "roundtrip",
+      grepl("one-way",length) ~ "one-way",
+      grepl("of trails",length) ~ "trails"),
+    
+    length_total = as.numeric(gsub("(\\d+[.]\\d+).*","\\1", length)) * ((trip == "one-way") + 1),
+    
+    gain = as.numeric(gain),
+    highpoint = as.numeric(highpoint),
+    
+    location_general = gsub("(.*)\\s[-][-].*","\\1",location)
+  )
+
+
+# Save a single object to a file
+saveRDS(clean_hike_data, "clean_hike_data.rds")
+# Restore it under a different name
+reupload_data <- readRDS("clean_hike_data.rds")
+
+# Save file as csv, this puts features into string
+fwrite(clean_hike_data, file ="myDT.csv")
+
+# Extracting all features of the data
+unique(unlist(clean_hike_data$features))
+# Extracting all regions in the data
+unique(clean_hike_data$location_general)
+
+# Example filtering by region and feature character vector in R
+filter.area.feature <- clean_hike_data %>%
+  filter(location_general == "Mount Rainier Area") %>%
+  dplyr::filter(grepl('Dogs not allowed', features))
+
+
